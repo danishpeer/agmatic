90dd436841c9739bb75c749ed8f77db645545e23
Danish Khursheed <pdkh100@gmail.com>
2025-03-15 17:56:01 +0530

    Integrated Clerk Auth

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import BreadcrumbHeader from '@/components/BreadcrumbHeader'
 import DesktopSidebar from '@/components/Sidebar'
 import { ModeToggle } from '@/components/ThemeModeToggle'
 import { Separator } from '@/components/ui/separator'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function layout({children} : {children: React.ReactNode}) {
@@ -11,7 +12,12 @@ function layout({children} : {children: React.ReactNode}) {
         <div className='flex flex-1 flex-col min-h-screen'>
             <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>
                 <BreadcrumbHeader />
-                <div className='gap-1 flex items-center'><ModeToggle/></div>
+                <div className='gap-1 flex items-center'>
+                    <ModeToggle/>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </div>
             </header>
             <Separator />
             <div className='overflow-auto'>
